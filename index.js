6213fbb1fa4973889b2883af86cb416ab840c670
keshav <tomarkeshavofficial@gmail.com>
2023-04-02 02:39:57 +0530

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const v = 5;
 
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://employee-management-website-two.vercel.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origiin, X-Requested-With, Content-Type, Accept"
