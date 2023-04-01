00a296270e71bc160c9b27c4ad670c9e3dc79e8d
keshav <tomarkeshavofficial@gmail.com>
2023-04-01 23:17:57 +0530

    .

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const express = require('express')
-const cors = require('cors');
 const app = express()
 const mongoDB = require("./db");
 mongoDB();
 const v = 5;
 
 
-app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://employee-management-website-two.vercel.app");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origiin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://employee-management-website-keshavtomar.vercel.app/");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origiin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://employee-management-website-git-master-keshavtomar.vercel.app/");
+    res.setHeader("Access-Control-Allow-Origin", "https://employee-management-website-two.vercel.app/fetch");
     res.header(
         "Access-Control-Allow-Headers",
         "Origiin, X-Requested-With, Content-Type, Accept"
