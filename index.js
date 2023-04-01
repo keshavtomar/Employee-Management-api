dc144ffa6e291f0a49de3021457399ec5ef5db0d
keshav <tomarkeshavofficial@gmail.com>
2023-04-02 00:10:13 +0530

    ..

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(cors({
     origin: 'https://employee-management-website-two.vercel.app'
 }));
 app.use((req, res, next) => {
+    console.log(req);
     res.setHeader("Access-Control-Allow-Origin", "https://employee-management-website-two.vercel.app");
+    res.header('Access-Control-Allow-Origin', 'https://employee-management-website-two.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header(
         "Access-Control-Allow-Headers",
         "Origiin, X-Requested-With, Content-Type, Accept"
