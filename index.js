b999d9d21a33f1291766d2098cd51f39a71011aa
keshav <tomarkeshavofficial@gmail.com>
2023-04-03 14:43:55 +0530

    ..

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ mongoDB();
 const v = 5;
 
 
-app.use((req, res, next) => {
-    res.setHeader('Access - Control - Allow - Origin', 'https://employee-management-website-two.vercel.app');
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origiin, X-Requested-With, Content-Type, Accept"
-    );
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://employee-management-website-two.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-})
+});
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(express.json());
 
